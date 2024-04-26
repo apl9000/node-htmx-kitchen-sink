eeb43be860fa71dbb07c583565185ff9e87ad585
aperkslabbe <andrewperkslabbe@gmail.com>
2024-04-26 14:27:17 -0400

    register partials in app bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, 'public'));
   app.setBaseViewsDir(join(__dirname, 'views'));
   app.setViewEngine('hbs');
+  hbs.registerPartials(join(__dirname, 'views/partials'));
   await app.listen(3000);
-  console.log(`Awaiting orders on ${await app.getUrl()}, sir!`);
+  console.log(`${await app.getUrl()} awaiting orders !`);
 }
 bootstrap();
